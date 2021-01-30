5cf1f56470ded47894503b310bde3949e9c5fe9e
thegecko <rob.moran@arm.com>
2021-01-30 11:57:04 +0000

    All tests passing!

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
  */
 
 #include <string.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
 #include <libopencm3/usb/usbd.h>
+#include <libopencm3/stm32/rcc.h>
 
 #define BULK_EP_MAXPACKET 64
+#define CONTROL_EP_PACKET 16
 
 static const struct usb_device_descriptor dev = {
     .bLength = USB_DT_DEVICE_SIZE,
@@ -19,7 +21,7 @@ static const struct usb_device_descriptor dev = {
     .bMaxPacketSize0 = BULK_EP_MAXPACKET,
     .idVendor = 0x59e3,
     .idProduct = 0x0a23,
-    .bcdDevice = 0x0200, // Version
+    .bcdDevice = 0x0110, // Version
     .iManufacturer = 1,
     .iProduct = 2,
     .iSerialNumber = 3,
@@ -88,45 +90,27 @@ static const struct usb_config_descriptor config = {
 
 static const char *usb_strings[] = {
     "Nonolith Labs",
-    "STM32F103 Example Device",
-    "DEMO",
+    "STM32F103 Test Device",
+    "TEST_DEVICE",
 };
 
-/* Buffer to be used for control requests. */
-uint8_t usbd_control_buffer[128];
-uint8_t testbuf[16];
-
-static void ep_1_cb(usbd_device *usbd_dev, uint8_t ep) {
-    char buf[BULK_EP_MAXPACKET];
-    usbd_ep_write_packet(usbd_dev, ep, buf, BULK_EP_MAXPACKET);
-}
-
-static void ep_2_cb(usbd_device *usbd_dev, uint8_t ep) {
-    char buf[BULK_EP_MAXPACKET];
-    usbd_ep_read_packet(usbd_dev, ep, buf, BULK_EP_MAXPACKET);
-}
-
-static void ep_4_cb(usbd_device *usbd_dev, uint8_t ep)
-{
-    (void)usbd_dev;
-    (void)ep;
-    // Do nothing
-}
+static usbd_device *test_dev;
+static uint8_t control_buf[CONTROL_EP_PACKET];
+static uint8_t poll_buf[BULK_EP_MAXPACKET];
 
 static enum usbd_request_return_codes control_callback(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
         uint16_t *len, void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req)) {
-
     (void)usbd_dev;
     (void)complete;
 
     switch(req->bRequest) {
         case 0x81: {
             if (req->bmRequestType & USB_REQ_TYPE_IN) {
-                memcpy(*buf, testbuf, 16);
-                *len = 16;
+                memcpy(*buf, control_buf, CONTROL_EP_PACKET);
+                *len = CONTROL_EP_PACKET;
             } else {
-                if (*len > 16) *len = 16;
-                memcpy(testbuf, *buf, *len);
+                if (*len > CONTROL_EP_PACKET) *len = CONTROL_EP_PACKET;
+                memcpy(control_buf, *buf, *len);
             }
 
             return USBD_REQ_HANDLED;
@@ -139,27 +123,34 @@ static enum usbd_request_return_codes control_callback(usbd_device *usbd_dev, st
 static void config_callback(usbd_device *usbd_dev, uint16_t wValue) {
     (void)wValue;
 
-    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_IN | 1, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, ep_1_cb);
-    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 2, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, ep_2_cb);
+    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_IN | 1, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
+    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 2, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
     usbd_ep_setup(usbd_dev, USB_REQ_TYPE_IN | 3, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
-    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 4, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, ep_4_cb);
+    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 4, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
 
     usbd_register_control_callback(usbd_dev, USB_REQ_TYPE_VENDOR, USB_REQ_TYPE_TYPE, control_callback);
 
-    // Preload data for polling
-    ep_1_cb(usbd_dev, USB_REQ_TYPE_IN | 1);
-
-    // Break ep 4 so it times out
+    // NAK endpoint 4 so it times out
     usbd_ep_nak_set(usbd_dev, USB_REQ_TYPE_OUT | 4, 1);
+
+	// SysTick interrupt every N clock pulses: set reload to N-1
+	systick_set_reload(99999);
+	systick_interrupt_enable();
+	systick_counter_enable();
 }
 
 int main(void) {
-    usbd_device *usbd_dev;
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_48MHZ]);
 
-    usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
-    usbd_register_set_config_callback(usbd_dev, config_callback);
-                
+    uint8_t usbd_control_buffer[128];
+    test_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
+    usbd_register_set_config_callback(test_dev, config_callback);
+
     while (1)
-        usbd_poll(usbd_dev);
+        usbd_poll(test_dev);
+}
+
+void sys_tick_handler(void) {
+	usbd_ep_write_packet(test_dev, USB_REQ_TYPE_IN | 1, poll_buf, BULK_EP_MAXPACKET);
+	usbd_ep_read_packet(test_dev, USB_REQ_TYPE_OUT | 2, poll_buf, BULK_EP_MAXPACKET);
 }
