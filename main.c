9f904266cf5ff5f91c99d81ec219c130212b323d
thegecko <rob.moran@arm.com>
2026-01-30 18:15:23 +0000

    Update firmware to ping-pong

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Rob Moran 2021 <github@thegecko.org>
+ * Copyright (c) Rob Moran 2026 <github@thegecko.org>
  */
 
 #include <string.h>
@@ -56,6 +56,20 @@ static const struct usb_endpoint_descriptor endp_bulk[] = {{
     .bmAttributes = USB_ENDPOINT_ATTR_BULK | USB_ENDPOINT_ATTR_NOSYNC | USB_ENDPOINT_ATTR_DATA,
     .wMaxPacketSize = BULK_EP_MAXPACKET,
     .bInterval = 0,
+}, {
+    .bLength = USB_DT_ENDPOINT_SIZE,
+    .bDescriptorType = USB_DT_ENDPOINT,
+    .bEndpointAddress = USB_REQ_TYPE_IN | 5,
+    .bmAttributes = USB_ENDPOINT_ATTR_BULK | USB_ENDPOINT_ATTR_NOSYNC | USB_ENDPOINT_ATTR_DATA,
+    .wMaxPacketSize = BULK_EP_MAXPACKET,
+    .bInterval = 0,
+}, {
+    .bLength = USB_DT_ENDPOINT_SIZE,
+    .bDescriptorType = USB_DT_ENDPOINT,
+    .bEndpointAddress = USB_REQ_TYPE_OUT | 6,
+    .bmAttributes = USB_ENDPOINT_ATTR_BULK | USB_ENDPOINT_ATTR_NOSYNC | USB_ENDPOINT_ATTR_DATA,
+    .wMaxPacketSize = BULK_EP_MAXPACKET,
+    .bInterval = 0,
 }};
 
 static const struct usb_interface_descriptor data_iface[] = {{
@@ -63,7 +77,7 @@ static const struct usb_interface_descriptor data_iface[] = {{
     .bDescriptorType = USB_DT_INTERFACE,
     .bInterfaceNumber = 0,
     .bAlternateSetting = 0,
-    .bNumEndpoints = 4,
+    .bNumEndpoints = 6,
     .bInterfaceClass = USB_CLASS_VENDOR,
     .bInterfaceSubClass = 0,
     .bInterfaceProtocol = 0,
@@ -97,6 +111,16 @@ static const char *usb_strings[] = {
 static usbd_device *test_dev;
 static uint8_t control_buf[CONTROL_EP_PACKET];
 static uint8_t poll_buf[BULK_EP_MAXPACKET];
+static uint8_t bulk_buf[BULK_EP_MAXPACKET];
+static volatile int ep_pending_len = 0;
+
+static void ep_out_cb(usbd_device *usbd_dev, uint8_t ep) {
+    (void)ep;
+    int len = usbd_ep_read_packet(usbd_dev, USB_REQ_TYPE_OUT | 4, bulk_buf, sizeof(bulk_buf));
+    if (len > 0) {
+        ep_pending_len = len;
+    }
+}
 
 static enum usbd_request_return_codes control_callback(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
         uint16_t *len, void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req)) {
@@ -123,17 +147,25 @@ static enum usbd_request_return_codes control_callback(usbd_device *usbd_dev, st
 static void config_callback(usbd_device *usbd_dev, uint16_t wValue) {
     (void)wValue;
 
+    // For polling
     usbd_ep_setup(usbd_dev, USB_REQ_TYPE_IN | 1, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
     usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 2, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
-    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_IN | 3, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
-    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 4, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
 
+        // For timeouts
+    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_IN | 3, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
+    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 4, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, ep_out_cb);
+
+    // For echoing back data received
+    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_IN | 5, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
+    usbd_ep_setup(usbd_dev, USB_REQ_TYPE_OUT | 6, USB_ENDPOINT_ATTR_BULK, BULK_EP_MAXPACKET, NULL);
+
+    // Control requests
     usbd_register_control_callback(usbd_dev, USB_REQ_TYPE_VENDOR, USB_REQ_TYPE_TYPE, control_callback);
 
-    // NAK endpoint 4 so it times out
-    usbd_ep_nak_set(usbd_dev, USB_REQ_TYPE_OUT | 4, 1);
+    // NAK endpoint 6 so it times out
+    usbd_ep_nak_set(usbd_dev, USB_REQ_TYPE_OUT | 6, 1);
 
-	// SysTick interrupt every N clock pulses: set reload to N-1
+    // SysTick interrupt every N clock pulses: set reload to N-1
 	systick_set_reload(99999);
 	systick_interrupt_enable();
 	systick_counter_enable();
@@ -146,8 +178,16 @@ int main(void) {
     test_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
     usbd_register_set_config_callback(test_dev, config_callback);
 
-    while (1)
+    while (1) {
         usbd_poll(test_dev);
+
+        if (ep_pending_len > 0) {
+            int wrote = usbd_ep_write_packet(test_dev, USB_REQ_TYPE_IN | 3, bulk_buf, ep_pending_len);
+            if (wrote > 0) {
+                ep_pending_len = 0;
+            }
+        }
+    }
 }
 
 void sys_tick_handler(void) {
